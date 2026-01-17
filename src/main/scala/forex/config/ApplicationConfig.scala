58929542f5bbb0230a501cb9d113f0239134897e
Robert Harley <reharley20@gmail.com>
2026-01-17 23:22:35 +0000

    config and domain

---
+++ src/main/scala/forex/config/ApplicationConfig.scala
@@ -0,0 +1,24 @@
+package forex.config
+
+import scala.concurrent.duration.FiniteDuration
+
+case class ApplicationConfig(
+    http: HttpConfig,
+    oneFrame: OneFrameConfig,
+    cache: CacheConfig
+)
+
+case class HttpConfig(
+    host: String,
+    port: Int,
+    timeout: FiniteDuration
+)
+
+case class OneFrameConfig(
+    baseUrl: String,
+    token: String
+)
+
+case class CacheConfig(
+    ttlSeconds: Int
+)
---
+++ src/main/scala/forex/domain/Currency.scala
@@ -0,0 +1,48 @@
+package forex.domain
+
+import cats.Show
+
+sealed trait Currency
+
+object Currency {
+  case object AUD extends Currency
+  case object CAD extends Currency
+  case object CHF extends Currency
+  case object EUR extends Currency
+  case object GBP extends Currency
+  case object NZD extends Currency
+  case object JPY extends Currency
+  case object SGD extends Currency
+  case object USD extends Currency
+
+  // All available currencies - single source of truth
+  val all: List[Currency] = List(AUD, CAD, CHF, EUR, GBP, NZD, JPY, SGD, USD)
+
+  implicit val show: Show[Currency] = Show.show {
+    case AUD => "AUD"
+    case CAD => "CAD"
+    case CHF => "CHF"
+    case EUR => "EUR"
+    case GBP => "GBP"
+    case NZD => "NZD"
+    case JPY => "JPY"
+    case SGD => "SGD"
+    case USD => "USD"
+  }
+
+  def fromString(s: String): Either[String, Currency] = s.toUpperCase match {
+    case "AUD"   => Right(AUD)
+    case "CAD"   => Right(CAD)
+    case "CHF"   => Right(CHF)
+    case "EUR"   => Right(EUR)
+    case "GBP"   => Right(GBP)
+    case "NZD"   => Right(NZD)
+    case "JPY"   => Right(JPY)
+    case "SGD"   => Right(SGD)
+    case "USD"   => Right(USD)
+    case invalid =>
+      val supported = all.map(show.show).mkString(", ")
+      Left(s"Invalid currency code: $invalid. Supported currencies: $supported")
+  }
+
+}
